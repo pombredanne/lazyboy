7f0110249ddcf302c73af61e431187f2d6f609b7
Ian Eure <ian.eure@gmail.com>
2009-10-22 12:41:25 -0700

    Add new iterators module. Move key range iterator to iterators.

---
+++ lazyboy/iterators.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# © 2009 Digg, Inc. All rights reserved.
+# Author: Ian Eure <ian@digg.com>
+#
+
+"""Iterator-based Cassandra tools."""
+
+from lazyboy.connection import get_pool
+
+from cassandra.ttypes import SlicePredicate, SliceRange, ConsistencyLevel
+
+
+def slice_iterator(key, **range_args):
+    """Return an iterator over a row."""
+
+    kwargs = {'start': "", 'finish': "",
+              'count': 100000, 'reversed': 0}
+    kwargs.update(range_args)
+
+    client = get_pool(key.keyspace)
+    res = client.get_slice(
+        key.keyspace, key.key, key,
+        SlicePredicate(slice_range=SliceRange(**kwargs)),
+        ConsistencyLevel.ONE)
+
+    return (corsc.column or corsc.super_column for corsc in res)
+
+
+def sparse_get(key, columns):
+    """Return an iterator over a specific set of columns."""
+
+    client = get_pool(key.keyspace)
+    res = client.get_slice(
+        key.keyspace, key.key, key, SlicePredicate(column_names=columns),
+        ConsistencyLevel.ONE)
+
+    return (corsc.column or corsc.super_column for corsc in res)
+
+
+def sparse_multiget(keys, columns):
+    """Return an iterator over a specific set of columns."""
+
+    first_key = iter(keys).next()
+    client = get_pool(first_key.keyspace)
+    row_keys = [key.key for key in keys]
+    res = client.multiget_slice(
+        first_key.keyspace, row_keys, first_key,
+        SlicePredicate(column_names=columns), ConsistencyLevel.ONE)
+
+    out = {}
+    for (row_key, cols) in res.iteritems():
+        out[row_key] = [corsc.column or corsc.super_column for corsc in cols]
+    return out
+
+
+def key_range(key, start="", finish="", count=100):
+    """Return an iterator over a range of keys."""
+    cas = get_pool(key.keyspace)
+    return cas.get_key_range(key.keyspace, key.column_family, start,
+                                  finish, count, ConsistencyLevel.ONE)
+
+
+def key_range_iterator(key, start="", finish="", count=100):
+    """Return an iterator which produces Key instances for a key range."""
+    return (key.clone(key=k) for k in key_range(key, start, finish, count))
