ba9e64e982a51b6d8c9b8144e7e3954d6ff524a5
Ian Eure <ian.eure@gmail.com>
2009-10-23 11:04:09 -0700

    Import iterators.

--- lazyboy/__init__.py
+++ lazyboy/__init__.py
@@ -11,3 +11,5 @@ from lazyboy.key import Key
 from lazyboy.record import Record
 from lazyboy.recordset import RecordSet, KeyRecordSet
 from lazyboy.view import View, PartitionedView
+from lazyboy.iterators import slice_iterator, sparse_get, sparse_multiget, \
+    key_range, key_range_iterator, pack, unpack
