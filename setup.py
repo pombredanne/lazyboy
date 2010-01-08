f929074f504884373978bf0784b47f1abc4d87c9
Ian Eure <ian.eure@gmail.com>
2010-01-08 14:10:24 -0800

    Update Cassandra dependency to svn897307.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name="Lazyboy",
       author_email="ian@digg.com",
       license="Three-clause BSD",
       keywords="database cassandra",
-      install_requires=['Thrift', 'Cassandra>=0.4.0'],
+      install_requires=['Thrift', 'Cassandra==svn897307'],
       zip_safe=False,
       tests_require=['nose', 'coverage>=3.2b1'],
       dependency_links=DEPS)
