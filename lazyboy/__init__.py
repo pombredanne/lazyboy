04198e5e202e1ae0b365023080a522fa331c5861
Ian Eure <ian.eure@gmail.com>
2010-01-15 10:40:41 -0800

    Import some more stuff in the main lazyboy module.

--- lazyboy/__init__.py
+++ lazyboy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# © 2009 Digg, Inc. All rights reserved.
+# © 2009, 2010 Digg, Inc. All rights reserved.
 # Author: Ian Eure <ian@digg.com>
 #
 
@@ -10,6 +10,7 @@ from lazyboy.connection import add_pool, get_pool
 from lazyboy.key import Key
 from lazyboy.record import Record, MirroredRecord
 from lazyboy.recordset import RecordSet, KeyRecordSet
-from lazyboy.view import View, PartitionedView
+from lazyboy.view import (View, PartitionedView, BatchLoadingView,
+                          FaultTolerantView)
 from lazyboy.iterators import slice_iterator, sparse_get, sparse_multiget, \
     key_range, key_range_iterator, pack, unpack, multigetterator
